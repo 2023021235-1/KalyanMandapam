1704b4ae45ec46ee63f4a226135af16485d8de3f
himanshuu932 <himanshuu932@gmail.com>
2025-11-15 21:59:36 +0530

    git push

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const { verifyToken, admin } = require('../middleware/authMiddleware'); // Assuming you have an admin middleware
 
 const adminAuthController = require('../controller/adminAuthController');
-// const { nameadmin } = require("../middleWare/nameadmin");
+// const { admin } = require("../middleWare/admin");
 
-router.post('/add-admin', verifyToken, nameadmin, adminAuthController.addAdmin);
-router.get('/admins', verifyToken, nameadmin, adminAuthController.getAllAdmins);
+router.post('/add-admin', verifyToken, admin, adminAuthController.addAdmin);
+router.get('/admins', verifyToken, admin, adminAuthController.getAllAdmins);
 // --- NEW ROUTES ADDED ---
-router.patch('/admin/:id', verifyToken, nameadmin, adminAuthController.updateAdmin);
-router.delete('/admin/:id', verifyToken, nameadmin, adminAuthController.deleteAdmin);
+router.patch('/admin/:id', verifyToken, admin, adminAuthController.updateAdmin);
+router.delete('/admin/:id', verifyToken, admin, adminAuthController.deleteAdmin);
 // 
 
 module.exports = router;
